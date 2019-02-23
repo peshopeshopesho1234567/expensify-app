ce4b421410534189a0fd990d0a24823fc444244f
Penev <Grigor.Penev@verint.com>
2019-02-23 10:01:10 +0200

    Removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -8,13 +8,11 @@ import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 
 const store = configureStore();
-store.dispatch(addExpense({ description: 'Ferrari 488', amount: 700000000 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 70000, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 43000 }));
 
 const jsx = (
   <Provider store={store}>
     <AppRouter/>
   </Provider>
 )
+
 ReactDOM.render(jsx, document.getElementById('app'));
