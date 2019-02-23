10d36d7ecd1526ff2e09c2622a2523203455b79a
Penev <Grigor.Penev@verint.com>
2019-02-23 14:51:27 +0200

    Fix improper expense date filter and default values for createdAt

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -2,7 +2,7 @@ import moment from 'moment';
 
 const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate}) => {
   return expenses.filter(expense => {
-    const createdAtMoment = moment(expense.createdAt);
+    const createdAtMoment = expense.createdAt ? moment(expense.createdAt) : moment();
     const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
     const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
     const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
