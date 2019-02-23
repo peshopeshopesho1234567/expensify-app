9c66794c9d1b679370f4666e9a9097e3bdbd2e71
Penev <Grigor.Penev@verint.com>
2019-02-23 10:00:24 +0200

    Revert "Revert commit"
    This reverts commit 318cdad72a8a554f2760e64ac60c927a2b8e98b3.

--- src/app.js
+++ src/app.js
@@ -4,8 +4,13 @@ import { Provider } from 'react-redux';
 
 import configureStore from './store/configureStore';
 import AppRouter from './routers/AppRouter';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
 
 const store = configureStore();
+store.dispatch(addExpense({ description: 'Ferrari 488', amount: 700000000 }));
+store.dispatch(addExpense({ description: 'Gas Bill', amount: 70000, createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 43000 }));
 
 const jsx = (
   <Provider store={store}>
