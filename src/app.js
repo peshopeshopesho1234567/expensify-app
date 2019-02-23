f1393fb2183f6dc0d2fbd2085e50c807b51cf995
Penev <Grigor.Penev@verint.com>
2019-02-23 14:06:40 +0200

    Fix app.js file formatting

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import configureStore from './store/configureStore';
-import AppRouter from './routers/AppRouter'; 
+import AppRouter from './routers/AppRouter';
 
 const store = configureStore();
 
